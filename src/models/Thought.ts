f56b17f43500297f81fc2635669c852f8474fa62
limboden <imbodenlu@gmail.com>
2024-11-20 18:14:11 -0500

    started on thought.ts

--- src/models/Thought.ts
+++ src/models/Thought.ts
@@ -1,38 +1,42 @@
 import { Schema, Document, ObjectId, Types } from 'mongoose';
 
 interface IResponse extends Document {
-  thoughtText: String;
-  responseBody: string;
-  username: string;
+  thoughtText: string; // must be between 1 and 280 chars
   createdAt: Date;
+  username: string;
+  reactions: ObjectId[];
 }
 
-const responseSchema = new Schema<IResponse>(
+const throughtSchema = new Schema<IResponse>(
   {
-    reactionId: {
-      type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId(),
-    },
-    responseBody: {
+    thoughtText: {
       type: String,
       required: true,
-      maxlength: 280,
-    },
-    username: {
-      type: String,
-      required: true,
+      minlength: 1,
+      maxlength: 280
     },
     createdAt: {
       type: Date,
       default: Date.now,
     },
-  },
-  {
-    toJSON: {
-      getters: true,
+    username: {
+      type: String,
+      required: true,
     },
-    id: false,
+    reactions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'reaction',
+      },
+    ],
   }
 );
 
-export default responseSchema;
+
+throughtSchema
+  .virtual('reactionCount')
+  .get(function (this: any) {
+    return `${this.reactions.length}`;
+  });
+
+export default throughtSchema;
